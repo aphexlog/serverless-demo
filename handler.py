66a0bbc7c1ddf5d32f053803fccd13b48058058b
ant <38507580+antch57@users.noreply.github.com>
2024-01-12 14:47:32 -0700

    add bucket and table

--- handler.py
+++ handler.py
@@ -1,10 +1,16 @@
 import json
-
+from os import environ
 
 def hello(event, context):
     body = {
         "message": "Go Serverless v3.0! Your function executed successfully!",
         "input": event,
     }
+
+    dynamoArn = environ.get("DYNAMODB_TABLE_NAME")
+    s3Arn = environ.get("S3_BUCKET_ARN")
+
+    print(dynamoArn)
+    print(s3Arn)
 
     return {"statusCode": 200, "body": json.dumps(body)}
